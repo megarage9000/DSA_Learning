5909e7808f8dad7fd4184fc81100c9f30111c446
John Ordoyo <gtom_1984@hotmail.ca>
2020-06-28 20:33:30 -0700

    DSA_Intro: Importance of DSA
    - Lesson Link: https://www.programiz.com/dsa/why-algorithms

--- main.cpp
+++ main.cpp
@@ -3,13 +3,28 @@
 #include <string>
 
 
+// DSA_Intro: Why DSA's are important
+// Lesson Link: https://www.programiz.com/dsa/why-algorithms
+
+// Why are Data Structures and Algorithms Important?
+// - Time
+//      - Allows programs to run faster
+// - Scalabilitiy
+//      - Can deal with larger problems
+// - Memory
+//      - Can use the least or use memory in
+//      the most efficient way possible
 
 double factorial(int num);
+double sumNaturalNumbers(int n);
 int getIntegerInput(std::string message);
 
 int main(){
-    std::cout << "--- Simple Factorial Program ---\n";
-    std::cout << factorial(getIntegerInput("Enter an integer: ")) << '\n';
+    // std::cout << "--- Simple Factorial Program ---\n";
+    // std::cout << factorial(getIntegerInput("Enter an integer: ")) << '\n';
+    
+    std::cout << "--- Sum Natural Numbers Program --- \n";
+    std::cout << sumNaturalNumbers(getIntegerInput("Enter a number: ")) << '\n';
 }
 
 int getIntegerInput(std::string message){
@@ -38,4 +53,10 @@ double factorial(int num){
         fact = fact * i;
     }
     return fact;
+}
+
+double sumNaturalNumbers(int n){
+    // Natural number series:
+    // https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+    return n * (n + 1)/2;
 }
